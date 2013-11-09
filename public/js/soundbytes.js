2cd0b91689502a2dc2a60bbb2aca27dfadbac58e
Tyler Hedrick <tylerhedrick@me.com>
2013-11-08 20:20:51 -0500

    doing user add things

--- public/js/soundbytes.js
+++ public/js/soundbytes.js
@@ -1,5 +1,17 @@
+Parse.initialize("BQmvWMb7YW13061P4ve9qsgwcIE1jM5oVg1p6P7P", "vJtu9l4rHykKATckBgfaPY3uBGRSX4c2AEKoqa8w");
+
 $(document).ready(function() {
-	$("#main-content").css('margin-top', $(".header-carousel").height() + 'px');
+	//$("#main-content").css('margin-top', $(".header-carousel").height() + 'px');
+	$("#user-submit").click(function() {
+		var Soundbyte = Parse.Object.extend("Bytes");
+		var soundbyte = new Soundbyte();
+		var values = $("#user-form").serializeArray();
+		for (var i = 0; i < values.length; i++) {
+			var val = values[i];
+			soundbyte.set(val.name, val.value);
+		}
+		soundbyte.save();
+	});
 });	
 
 $(window).resize(function() {
