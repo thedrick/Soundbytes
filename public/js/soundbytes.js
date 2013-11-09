23632bcd489c11371a1e389ef8d02656f4571da7
Boldak <boldak13@gmail.com>
2013-11-08 21:52:35 -0500

    get rid of weird margin-top

--- public/js/soundbytes.js
+++ public/js/soundbytes.js
@@ -1,11 +1,8 @@
 Parse.initialize("BQmvWMb7YW13061P4ve9qsgwcIE1jM5oVg1p6P7P", "vJtu9l4rHykKATckBgfaPY3uBGRSX4c2AEKoqa8w");
 
 $(document).ready(function() {
-	//$("#main-content").css('margin-top', $(".header-carousel").height() + 'px');
-	$("#main-content").css('margin-top', $(".header-carousel").height() + 'px');
 	$('.toggle-menu').jPushMenu();
 });	
 
 $(window).resize(function() {
-	//$("#main-content").css('margin-top', $(".header-carousel").height() + 'px');
 });
